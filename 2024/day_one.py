68f23cb223cd13c09237d00026aaed84c6a14160
Jack Allen <jack.allen.web@gmail.com>
2024-12-02 15:44:24 +0000

    Set up app structure

---
+++ 2024/day_one.py
@@ -0,0 +1,5 @@
+"""Day one"""
+
+def part_one():
+    """Part one"""
+    print("part one")
---
+++ main.py
@@ -0,0 +1,4 @@
+"""Main file for advent of code"""
+
+if __name__ == '__main__':
+    print('advent_of_code start up')

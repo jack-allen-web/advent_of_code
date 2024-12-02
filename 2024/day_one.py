2ce85b7bffc145a901f7817b81fca8f5b1f18cb1
Jack Allen <jack.allen.web@gmail.com>
2024-12-02 16:12:16 +0000

    Add instruction set to project

--- 2024/day_one.py
+++ 2024/day_one.py
@@ -1,5 +1,13 @@
 """Day one"""
 
+INPUT_PATH = "./inputs/day_one.txt"
+
 def part_one():
     """Part one"""
-    print("part one")
+    with open(INPUT_PATH, mode="r", encoding="UTF-8") as input_file:
+
+        print(input_file.readlines())
+
+
+if __name__ == '__main__':
+    part_one()
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 Repository containing my solutions for the advent of code challenges.
 
 ## Project Structure
-    - 2024/      - Contains solutions for 2024
+    2024/      Contains solutions for 2024

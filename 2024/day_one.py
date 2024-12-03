d6d6e0f93884b206aff9080e28996a320ff79652
Jack Allen <jack.allen.web@gmail.com>
2024-12-03 18:07:37 +0000

    Next day

--- 2024/day_one.py
+++ 2024/day_one.py
@@ -5,8 +5,9 @@ INPUT_PATH = "./inputs/day_one.txt"
 def part_one():
     """Part one"""
     with open(INPUT_PATH, mode="r", encoding="UTF-8") as input_file:
+        lines = [line.strip() for line in input_file.readlines()]
 
-        print(input_file.readlines())
+        print(lines)
 
 
 if __name__ == '__main__':
